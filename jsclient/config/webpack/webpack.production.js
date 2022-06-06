f181ffdac551f58f0ba0e0035568b298b1ed5ddc
Jeff McBride <mcbridejc@gmail.com>
2022-06-06 12:09:24 -0700

    Fixes to build process required after upgrade to webpack5

--- jsclient/config/webpack/webpack.production.js
+++ jsclient/config/webpack/webpack.production.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const paths = require('./paths');
 
@@ -15,14 +15,14 @@ module.exports = {
         chunkFilename: 'js/[name]-[contenthash:8].js'
     },
     plugins: [
-        new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
+        new webpack.ids.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly
         new CleanWebpackPlugin(),
         new Dotenv({
-            path: paths.envProdPath, // Path to .env.production file 
+            path: paths.envProdPath, // Path to .env.production file
             expand: true
         }),
         new Dotenv({
-            path: paths.envPath, // Path to .env file 
+            path: paths.envPath, // Path to .env file
             expand: true
         }),
         new MiniCssExtractPlugin({
@@ -40,18 +40,27 @@ module.exports = {
                 vendor: {
                     test: /[\\/]node_modules[\\/]/,
                     name(module) {
+                        // With webpack5, the test condition above does not seem to preclude
+                        // this function being called with modules that are not from node_modules
+
                         // get the name. E.g. node_modules/packageName/not/this/part.js
                         // or node_modules/packageName
-                        const packageName = module.context.match(
+                        const match = module.context.match(
                             /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-                        )[1];
+                        );
 
-                        // npm package names are URL-safe, but some servers don't like @ symbols
-                        return `vendor.${packageName.replace('@', '')}`;
+                        if(match) {
+                            const packageName = match[1];
+                            // npm package names are URL-safe, but some servers don't like @ symbols
+                            return `vendor.${packageName.replace('@', '')}`;
+                        } else {
+                            return false;
+                        }
                     }
-                }
+                },
+                default: false
             }
         },
-        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+        minimizer: [new TerserPlugin({}), new CssMinimizerPlugin({})]
     }
 };
